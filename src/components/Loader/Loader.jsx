06a55760cf4ceee6eab54bc3ea18d93724160af9
LiudmylaHorobets <horobets1323@gmail.com>
2023-10-19 10:43:34 +0300

    refactoring loader component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,19 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { MagnifyingGlass } from 'react-loader-spinner';
 
-export class MyLoader extends Component {
-  render() {
-    return (
-      <div className="MyLoader">
+export function MyLoader() {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setVisible(false);
+    }, 5000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div className="MyLoader">
+      {visible && (
         <MagnifyingGlass
           visible={true}
           height="160"
@@ -15,7 +24,7 @@ export class MyLoader extends Component {
           glassColor="#e0e0e0"
           color="#3f51b5"
         />
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
